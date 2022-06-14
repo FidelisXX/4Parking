128d329b0eaad06fc1c29479d200a85b31b03be5
Davi <davifidelis78@gmail.com>
2022-06-14 19:43:01 -0300

    make it better

--- php/conexao.php
+++ php/conexao.php
@@ -2,7 +2,7 @@
 $mysql_servidor="us-cdbr-east-05.cleardb.net"; // Infos about the connection SQL.
 $mysql_usuario="b15089780828d0"; // login of user SQL database
 $mysql_senha="05bb3e24"; // password of user SQL database
-$mysql_database="heroku_a053a55912a00a8"; // name of SQL database
+$mysql_database="heroku_a053a55912a00a8"; // name of SQL databasea
 
 //$mysql_servidor="localhost"; // Infos about the connection SQL.
 //$mysql_usuario="Natan"; // login of user SQL database
